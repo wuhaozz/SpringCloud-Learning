16a1de534ce58ec83fc869932fcd3838550e69da
Wu Hao <405043046@qq.com>
2018-05-11 00:06:09 +0800

    first commit

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/wuhaozz/eurekaconsumerribbonhystrix/EurekaConsumerRibbonHystrixApplication.java
@@ -0,0 +1,26 @@
+package com.wuhaozz.eurekaconsumerribbonhystrix;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerRibbonHystrixApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaConsumerRibbonHystrixApplication.class).web(true).run(args);
+    }
+
+}
